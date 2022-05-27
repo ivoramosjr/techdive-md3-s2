fffaaf05961319df827d13f9b7f8a46e7fb65255
tiagoamp <tiagoamp@gmail.com>
2022-05-26 23:35:18 -0300

    Reset do projeto

--- src/main/java/org/techdive/InscricaoApplication.java
+++
@@ -1,9 +0,0 @@
-package org.techdive;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class InscricaoApplication extends Application {
-
-}
